c8488afcac99f8aa4f1e2d26564881a90316c69a
Giorgio <giorgioce@outlook.com>
2020-12-09 16:21:34 +0000

    ready to deploy

--- app.js
+++ app.js
@@ -24,9 +24,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/spends', spendsRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/spends', spendsRouter);
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
