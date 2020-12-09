790792f62b5ad78bcd17aeeafdb81e134b4cc856
Giorgio <giorgioce@outlook.com>
2020-12-09 17:43:26 +0000

    without start script in json

--- app.js
+++ app.js
@@ -53,7 +53,8 @@ mongoose.connect(process.env.DB_CONNECTION, { useUnifiedTopology: true, useNewUr
 () => console.log('connected to DB!'));
 
 // start the server listening for requests
-app.listen(process.env.PORT || 3000,
-	() => console.log("Server is running..."));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 module.exports = app;
